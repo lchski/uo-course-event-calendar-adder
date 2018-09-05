72d5205d3dbe53e48559bb937e501ffae93b7717
Lucas Cherkewski <lucas@ecustom.ca>
2018-09-05 15:38:15 -0400

    progress!

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 * importance
-    * 0: lowest importance, creates a "D!" event the day-of
-    * 1: low importance, creates a "D!!" event the day-of, and a "!" event the week before
-    * 2: middling importance, creates a "D!!" event the day-of, a "!" event the week before, and a "" event two weeks before
-    * 3: higher importance, creates a "D!!!" event the day-of, a "!!" event the week before, a "!" event two weeks before, and a "" event three weeks before
-    * 4: highest importance, creates a "D!!!" event the day-of, a "!!" event the week before, a "!" event two weeks before, a "" event three weeks before, and a "" event four weeks before
+    * 1: lowest importance, creates a "D!" event the day-of
+    * 2: low importance, creates a "D!!" event the day-of, and a "!" event the week before
+    * 3: middling importance, creates a "D!!" event the day-of, a "!" event the week before, and a "" event two weeks before
+    * 4: higher importance, creates a "D!!!" event the day-of, a "!!" event the week before, a "!" event two weeks before, and a "" event three weeks before
+    * 5: highest importance, creates a "D!!!" event the day-of, a "!!" event the week before, a "!" event two weeks before, a "" event three weeks before, and a "" event four weeks before
--- transform.R
+++ transform.R
@@ -1,3 +1,66 @@
+library(xfun)
 library(tidyverse)
 
-input_events <- read_csv("course-events.csv")
+inputEvents <- read_csv("course-events.csv")
+
+convertNumberOfWeeksToHumanString <- function(numberOfWeeks) {
+  convertedString <- ""
+  
+  if (numberOfWeeks == 0) {
+    return(convertedString)
+  } else if (numberOfWeeks == 1) {
+    convertedString <- paste(numbers_to_words(numberOfWeeks), "week")
+  } else {
+    convertedString <- paste(numbers_to_words(numberOfWeeks), "weeks")
+  }
+  
+  return(paste("(", convertedString,")", sep = ""))
+}
+
+generateSubject <- function(course, event, prefix, numberOfWeeks = 0) {
+  trimws(paste(prefix, " ", course, ": ", event, " ", convertNumberOfWeeksToHumanString(numberOfWeeks), sep = ""))
+}
+
+eventVector <- function(subject, date) {
+  tibble(subject, startDate = date, endDate = date, allDay = TRUE)
+}
+
+processEvent <- function(course, date, event, importance) {
+  switch(importance,
+    {
+      list(
+        eventVector(generateSubject(course, event, "D!"), date)
+      )
+    },
+    {
+      list(
+        eventVector(generateSubject(course, event, "D!!"), date),
+        eventVector(generateSubject(course, event, "!", 1), date - 7)
+      )
+    },
+    {
+      list(
+        eventVector(generateSubject(course, event, "D!!"), date),
+        eventVector(generateSubject(course, event, "!", 1), date - 7),
+        eventVector(generateSubject(course, event, "", 2), date - 14)
+      )
+    },
+    {
+      list(
+        eventVector(generateSubject(course, event, "D!!!"), date),
+        eventVector(generateSubject(course, event, "!!", 1), date - 7),
+        eventVector(generateSubject(course, event, "!", 2), date - 14),
+        eventVector(generateSubject(course, event, "", 3), date - 21)
+      )
+    },
+    {
+      list(
+        eventVector(generateSubject(course, event, "D!!!"), date),
+        eventVector(generateSubject(course, event, "!!", 1), date - 7),
+        eventVector(generateSubject(course, event, "!", 2), date - 14),
+        eventVector(generateSubject(course, event, "", 3), date - 21),
+        eventVector(generateSubject(course, event, "", 4), date - 28)
+      )
+    }
+  )
+}
